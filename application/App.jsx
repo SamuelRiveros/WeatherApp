78ab0591a6c05eb042877288e70e2e9bf5642d81
SamuelRiveros <156517817+SamuelRiveros@users.noreply.github.com>
2024-10-21 07:29:52 -0500

    feat: :heavy_plus_sign: Dependencias instaladas : tailwindcss, express, cors, etc. . . , pagina funcional con estas.

---
+++ application/App.jsx
@@ -0,0 +1,22 @@
+// import React from 'react';
+import { useState } from 'react'
+import './App.css'
+
+import { Home } from './pages/Home'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }
+])
+
+
+export function App() {
+  return(
+    <RouterProvider router={routes}/>
+  )
+}
+
+export default App
---
+++ application/pages/Home.jsx
@@ -0,0 +1,7 @@
+export function Home() {
+    return(
+        <main className="bg-white">
+            <p className="text-black">Holas</p>
+        </main>
+    )
+}
